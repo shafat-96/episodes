d4710fe400cc0edd227e21665ce57b95881e2ad6
Durgesh Kumar Dwivedi <durgeshdwivedi81@gmail.com>
2025-02-27 19:54:41 +0530

    check

--- src/providers/anime/animekai.ts
+++ src/providers/anime/animekai.ts
@@ -23,7 +23,25 @@ export class AnimeKaiProvider extends Provider {
   }
 
   async getMapping(title: ITitle): Promise<Mappings> {
-    const { data } = await axios.get("https://animekai.to")
+    const { data } = await axios.get("https://animekai.to", {
+      headers: {
+        authority: "animekai.to",
+        accept: "application/json, text/javascript, */*; q=0.01",
+        "accept-language": "en-US,en;q=0.9",
+        dnt: "1",
+        "sec-ch-ua":
+          '"Not A(Brand";v="99", "Microsoft Edge";v="121", "Chromium";v="121"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Windows"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-origin",
+        "x-requested-with": "XMLHttpRequest",
+        referer: "https://animekai.to/",
+        "user-agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+      },
+    });
     console.log(data);
     try {
       // Run searches in parallel
